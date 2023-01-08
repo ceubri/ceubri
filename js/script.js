b2757166320e77eeee483e39f6264e9a09927f58
ceubri <ceubrak@yahoo.fr>
2023-01-08 16:41:32 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-ceubri-portfolio-perso-pro-butique-etsy","displayName":"ceubri | portfolio perso pro butique etsy","link":{"linkType":"LinkTypePage","href":"#!page-ceubri-portfolio-perso-pro-butique-etsy"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
